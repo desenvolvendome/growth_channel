092d366bf50a2086841568eef997127c851937a4
Cauê Santos <caah.sfc@live.com>
2020-10-14 22:07:04 -0300

    feat: solicitando ao usuário o nome do arquivo a ser lido (tornando dinâmico)

--- OrganicViews.rb
+++ OrganicViews.rb
@@ -1,8 +1,13 @@
 require 'csv'
 class OrganicViews
+    puts "Welcome! To read your file successfully, it must be in the same folder as the script!"
+   
+    puts "Enter .CSV file name:"
+    file = gets.strip + ".csv"
+
     totalViews = 0;
 
-    report = CSV.read("organicViewsReport.csv")
+    report = CSV.read(file)
 
     for element in report
         totalViews += element[8].to_i
