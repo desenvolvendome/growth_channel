98f841eec3f51d5463fbe329de996d70e7864689
Marco Castro <marcodotcastro@gmail.com>
2020-10-18 07:36:25 -0300

    feat(youtube): adicionado contador total de views

--- spec/youtube/view_spec.rb
+++ spec/youtube/view_spec.rb
@@ -3,9 +3,9 @@ require 'growth_channel/youtube/view'
 RSpec.describe "Youtube::View" do
 
   it "count views" do
-    count = Youtube::View.new.read
+    total_views = Youtube::View.new("spec/suports/report_youtube_sample.csv").count
 
-    expect(count).to eq(283+930+537+154+67+13+85)
+    expect(total_views).to eq(283 + 930 + 537 + 154 + 67 + 13 + 85)
   end
 
 end
