c0dfbe4e9113c3fbd11c669abff662ffebc1e86b
Marco Castro <marcodotcastro@gmail.com>
2020-10-18 09:00:50 -0300

    test(view): adicionado total de visualizações por vídeo

--- spec/youtube/view_spec.rb
+++ spec/youtube/view_spec.rb
@@ -10,6 +10,15 @@ RSpec.describe "Youtube::View" do
       expect(total_views).to eq(283 + 930 + 537 + 154 + 67 + 13 + 85)
     end
 
+    it "total_views_per_video" do
+      total_views_per_video = Youtube::View.new("spec/suports/report_youtube_sample.csv").total_views_per_video
+      result = [
+          {id_video_principal: 'v1', views: 283 + 930},
+          {id_video_principal: 'v10', views: 537 + 154 + 67 + 13 + 85}
+      ]
+      expect(total_views_per_video).to eq(result)
+    end
+
   end
 
 
