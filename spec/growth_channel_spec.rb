c88efc2d910c5cfa5900e3506451ff1de817c74a
Marco Castro <marcodotcastro@gmail.com>
2020-10-18 12:48:15 -0300

    test: remove sample test

--- spec/growth_channel_spec.rb
+++ spec/growth_channel_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe GrowthChannel do
   it "has a version number" do
     expect(GrowthChannel::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
