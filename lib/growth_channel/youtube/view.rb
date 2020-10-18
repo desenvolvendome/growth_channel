815ef6ba50161f54df636be24af6bb30c839f611
Marco Castro <marcodotcastro@gmail.com>
2020-10-18 07:36:25 -0300

    feat(youtube): adicionado contador total de views

--- lib/growth_channel/youtube/view.rb
+++ lib/growth_channel/youtube/view.rb
@@ -1,7 +1,20 @@
+require 'csv'
+
 module Youtube
   class View
-    def read
-      2069
+
+    def initialize(file_path)
+      @file_path = file_path
+    end
+
+    def count
+      reports = CSV.read(@file_path, headers: true)
+      total_views = 0
+      reports.each do |row|
+        views = row["Views"].to_i
+        total_views += views
+      end
+      total_views
     end
   end
 end
