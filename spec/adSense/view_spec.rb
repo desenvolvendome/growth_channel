9a30e635557cd72d6c21f1a57f9b9b875b7c8633
Cauê Santos <caah.sfc@live.com>
2020-10-26 10:43:44 -0300

    test: adding test to validate the method total_internal_video_views

--- spec/adSense/view_spec.rb
+++ spec/adSense/view_spec.rb
@@ -13,6 +13,25 @@ RSpec.describe "AdSense::View" do
       expect(total_views_per_video).to eq(result)
     end
 
+    it "total_internal_videos_views" do
+      groups = AdSense::View.new("spec/suports/report_adsense.csv").total_internal_video_views
+      result = [
+          {id_video: 'v10.0', views: 451},
+          {id_video: 'v13.0', views: 444},
+          {id_video: 'v11.0', views: 463},
+          {id_video: 'v12.0', views: 455},
+          {id_video: 'v7.0', views: 404},
+          {id_video: 'v8.0', views: 398},
+          {id_video: 'v9.0', views: 442},
+          {id_video: 'v14.0', views: 455},
+          {id_video: 'v15.0', views: 468},
+          {id_video: 'v16.0', views: 445},
+          {id_video: 'v17.0', views: 299},
+          {id_video: 'v6.0', views: 2480}
+        ]
+      expect(groups).to match_array(result)
+    end
+
   end
 
 end
