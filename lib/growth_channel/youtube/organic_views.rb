97984f7bf46666e4962a3344bb1f12599acb9f2c
Cauê Santos <caah.sfc@live.com>
2020-10-27 08:46:42 -0300

    feat: including methods for extracting total organic views

---
+++ lib/growth_channel/youtube/organic_views.rb
@@ -0,0 +1,43 @@
+require 'csv'
+
+module OrganicViews
+  class View
+
+    def initialize(file_path)
+      @file_path = file_path
+    end
+
+    def total_views_per_video
+      total_per_video = []
+      videos_per_version.each do |version|
+        id_main_video = version.first[:id_main_video]
+        sum_views = version.inject(0) { |sum, hash| sum + hash[:views] }
+
+        total_per_video.push({id_main_video: id_main_video, views: sum_views})
+      end
+      total_per_video
+    end
+
+    private
+
+    def read_per_video
+      report = CSV.read(@file_path, headers: true)
+      main_videos = []
+      report.each do |row|
+        id_main_video = row["Video title"].split(":").first #v1.0 #v1.1
+        test = id_main_video.split(".").last.to_i
+        if test == 0
+          views = row["Views"].to_i
+          main_videos.push({id_main_video: id_main_video, views: views})
+        end
+      end
+      main_videos
+    end
+    
+
+    def videos_per_version
+      read_per_video.group_by { |h| h[:id_main_video] }.values
+    end
+
+  end
+end
