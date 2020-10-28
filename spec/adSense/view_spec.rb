16878f80d6c43f7a96d449537b3a8c747a2e83dc
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-10-28 00:20:20 -0300

    refactor: rename methods and create method group_videos_by_tag

--- spec/adSense/view_spec.rb
+++ spec/adSense/view_spec.rb
@@ -1,26 +1,23 @@
 require 'growth_channel/adSense/view'
 
-RSpec.describe "AdSense::View" do
-
-  context "count" do
-  
-    it "total_views_per_video" do
-      total_views_per_video = AdSense::View.new("spec/suports/report_adsense_sample.csv").total_views_per_video
+RSpec.describe 'AdSense::View' do
+  context 'count' do
+    it 'total_views_per_video' do
+      total_views_per_video = AdSense::View.new('spec/suports/report_adsense_sample.csv').total_views_per_video
       result = [
-          {id_video_principal: 'v1', views: 411 + 301},
-          {id_video_principal: 'v10', views: 451 + 107 + 0 + 37}
+        { id_video_principal: 'v1', views: 411 + 301 },
+        { id_video_principal: 'v10', views: 451 + 107 + 0 + 37 }
       ]
       expect(total_views_per_video).to eq(result)
     end
 
-    it "total_views_per_video_ideias" do
-      groups = AdSense::View.new("spec/suports/report_adsense_sample.csv").total_views_per_video_ideias
+    it 'total_views_per_video_ideias' do
+      groups = AdSense::View.new('spec/suports/report_adsense_sample.csv').get_videos_ideias
       result = [
-          {id_video_principal: 'v1', views: 411 + 301},
-          {id_video_principal: 'v10', views: 107 + 0 + 37}
+        { id_video_principal: 'v1', views: 411 + 301 },
+        { id_video_principal: 'v10', views: 107 + 0 + 37 }
       ]
       expect(groups).to match_array(result)
     end
   end
-
 end
