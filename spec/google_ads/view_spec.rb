987756552d3a7e34b381425f1d59fb6f906ca924
Emerson <eemr3@yahoo.com.br>
2020-10-31 14:02:26 -0300

    fix: atualizando a minha brache master com a master do projeto

--- spec/google_ads/view_spec.rb
+++ spec/google_ads/view_spec.rb
@@ -24,8 +24,8 @@ RSpec.describe "GoogleAds::View" do
       groups = GoogleAds::View.new('spec/suports/report_adsense_sample.csv').total_views_video_externo
 >>>>>>> 56edde726321e8ce26bc86703b404b57c822a881
       result = [
-          {id_video_principal: 'v1', views: 411 + 301},
-          {id_video_principal: 'v10', views: 107 + 0 + 37}
+        {id_video_principal: 'v1', views: 411 + 301},
+        {id_video_principal: 'v10', views: 107 + 0 + 37}
       ]
       expect(groups).to match_array(result)
     end
@@ -33,10 +33,20 @@ RSpec.describe "GoogleAds::View" do
     it 'total_views_per_video_ideias' do
       groups = GoogleAds::View.new('spec/suports/report_adsense_sample.csv').get_videos_ideias
       result = [
-          { id_video_principal: 'v1', views: 411 + 301 },
-          { id_video_principal: 'v10', views: 107 + 0 + 37 }
+        { id_video_principal: 'v1', views: 411 + 301 },
+        { id_video_principal: 'v10', views: 107 + 0 + 37 }
       ]
       expect(groups).to match_array(result)
     end
+
+    it 'cost_benefit_per_video' do
+      groups = GoogleAds::View.new('spec/suports/report_adsense_sample.csv').cost_benefit_per_video
+      result = [
+        { id_video_principal: 'v1', cost_benefit: (((712 + (46.43 * 2) + (30.92 * 4) + (25.17 * 5) + (17.97 * 3))/15)/ 10.05).floor(2)},
+        { id_video_principal: 'v10', cost_benefit: (((595 + (29.519 * 2) + (17.27 * 4) + (10.07 * 5) + (5.5 * 3))/15)/ 6.96).floor(2)}
+      ]
+      expect(groups).to match_array(result)
+    end
+
   end
 end
