75899b63bc35e16e6a240af3a6c120dba7886f64
Emerson <eemr3@yahoo.com.br>
2020-10-31 14:09:15 -0300

    fix: corrigindo arquivo spec/google_ads/view_spec.rb

--- spec/google_ads/view_spec.rb
+++ spec/google_ads/view_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe "GoogleAds::View" do
   context "count" do
 
     it "total_views_per_video" do
-<<<<<<< HEAD
+
       total_views_per_video = GoogleAds::View.new("spec/suports/report_adsense_sample.csv").total_views_per_video
-=======
+
       total_views_per_video = GoogleAds::View.new('spec/suports/report_adsense_sample.csv').total_views_per_video
->>>>>>> 56edde726321e8ce26bc86703b404b57c822a881
+
       result = [        
         {id_video_principal: 'v1', views: 411 + 301},
         {id_video_principal: 'v10', views: 451 + 107 + 0 + 37},
@@ -18,11 +18,11 @@ RSpec.describe "GoogleAds::View" do
     end
     
     it "total_views_video_externo" do
-<<<<<<< HEAD
+
       groups = GoogleAds::View.new("spec/suports/report_adsense_sample.csv").total_views_video_externo
-=======
+
       groups = GoogleAds::View.new('spec/suports/report_adsense_sample.csv').total_views_video_externo
->>>>>>> 56edde726321e8ce26bc86703b404b57c822a881
+
       result = [
         {id_video_principal: 'v1', views: 411 + 301},
         {id_video_principal: 'v10', views: 107 + 0 + 37}
