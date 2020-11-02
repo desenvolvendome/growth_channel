847d0d252f743e666969e761575df0dbb59c551f
Cauê Santos <caah.sfc@live.com>
2020-11-02 15:11:37 -0300

    refactor: changing CSV file

--- lib/growth_channel/util/manage_csv.rb
+++ lib/growth_channel/util/manage_csv.rb
@@ -2,12 +2,12 @@ require 'csv'
 class ManageCSV
 
     def self.read_csv_youtube
-        @file_path = "spec/suports/report_youtube_sample.csv"
+        @file_path = "spec/suports/report_youtube.csv"
         return CSV.read(@file_path, headers: true)
     end
 
     def self.read_csv_adsense
-        @file_path = "spec/suports/report_adsense_sample.csv"
+        @file_path = "spec/suports/report_adsense.csv"
         return CSV.read(@file_path, headers: true)
     end
 
