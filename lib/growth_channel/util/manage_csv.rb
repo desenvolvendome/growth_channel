3ba36f29eb236ccd3e29e4ed19359657ea1d56d7
Arlisson Jaques <arlissonjaques@gmail.com>
2020-11-04 07:02:19 -0300

    feat: add validation of csv youtube video names

--- lib/growth_channel/util/manage_csv.rb
+++ lib/growth_channel/util/manage_csv.rb
@@ -2,8 +2,16 @@ require 'csv'
 class ManageCSV
 
     def self.read_csv_youtube
-        @file_path = "spec/suports/report_youtube.csv"
-        return CSV.read(@file_path, headers: true)
+        @file_path = "spec/suports/invalid_video_title_youtube.csv"
+        reports = CSV.read(@file_path, headers: true)
+
+        valid_format = /^v\d*\.\d*\.?\d\:.+/
+        reports.each do |row|
+            unless valid_format.match(row['Video title'])
+                return "'#{row['Video title']}': It is not in the correct pattern"
+            end
+        end
+        reports
     end
 
     def self.read_csv_adsense
