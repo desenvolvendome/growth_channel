d37d704ce581ed98fe943f914e41289be2dc5af5
Cauê Santos <caah.sfc@live.com>
2020-11-09 21:04:07 -0300

    refactor: changing error messages

--- lib/growth_channel/util/manage_csv.rb
+++ lib/growth_channel/util/manage_csv.rb
@@ -4,15 +4,19 @@ class ManageCSV
     def self.read_csv_youtube
         @file_path = "spec/suports/report_youtube.csv"
         report = CSV.read(@file_path, headers: true)
+
         if validate_youtube_headers(report)
             report.each do |row|
                 if !row["Views"].match("[0-9]+")
-                    raise ArgumentError.new('Formato inválido.')  
+                    raise ArgumentError.new('Tipo de dado incorreto, esperado: númerico.')  
                 end
-            end                      
+            end      
+            
+            return report
+        
+        else 
+            raise ArgumentError.new('É necessário que o CSV contenha os formatos: [Video title] e [Views]')
         end
-
-        return report  
             
     end
 
@@ -22,13 +26,9 @@ class ManageCSV
     end
 
     def self.validate_youtube_headers(report)
-        validate = false 
-
         if report.headers.include? "Video title" 
             if report.headers.include? "Views"
                 validate = true
-            else
-                raise ArgumentError.new('É necessário que o CSV contenha os formatos: [Video title] e [Views]')
             end
         end
 
