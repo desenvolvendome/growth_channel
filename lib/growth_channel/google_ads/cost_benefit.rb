4bdd99704aaecf3b7f34211001d958a94c12e414
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-11-19 22:10:16 -0300

    refactor(cost_benefit): refactor based in rubycritic small changes

--- lib/growth_channel/google_ads/cost_benefit.rb
+++ lib/growth_channel/google_ads/cost_benefit.rb
@@ -1,23 +1,24 @@
 require_relative 'video.rb'
 
 module GoogleAds
+  # this class calculate the score of benefit cost
   class CostBenefit < Video
 
     def by_tag(tag, sorted: true)
-      per_video(sortIncreasing: sorted, filterTag: tag)
+      per_video(sort_increasing: sorted, filter_tag: tag)
     end
 
-    def per_video(sortIncreasing: false, filterTag: '')
-      group_videos_cost_benefit = []
-      group_main_video_info(filterTag: filterTag).each do |video|
+    def per_video(sort_increasing: false, filter_tag: '')
+      group_videos = []
+      group_main_video_info(filterTag: filter_tag).each do |video|
         id_video_principal = video[:id_video_principal]
         cost_benefit = video[:views] + (video[:watched_25] * 2) + (video[:watched_50] * 4) + (video[:watched_75] * 5) + (video[:watched_100] * 3)
         cost_benefit /= 15 # soma dos pesos
         cost_benefit /= video[:cost]
         cost_benefit = cost_benefit.floor(2)
-        group_videos_cost_benefit.push({id_video_principal: id_video_principal, cost_benefit: cost_benefit})
+        group_videos.push({id_video_principal: id_video_principal, cost_benefit: cost_benefit})
       end
-      sortIncreasing ? group_videos_cost_benefit.sort_by { |s| s[:cost_benefit] }.reverse : group_videos_cost_benefit
+      sort_increasing ? group_videos.sort_by { |element| element[:cost_benefit] }.reverse : group_videos
     end
   end
 end
