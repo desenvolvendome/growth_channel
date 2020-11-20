593274a0b8e52a46ec8190074202e0c2dd362058
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-11-20 00:19:28 -0300

    refactor(cost_benefit_spec): simplifying the test

--- spec/google_ads/cost_benefit_spec.rb
+++ spec/google_ads/cost_benefit_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe 'GoogleAds::CostBenefit' do
     it 'cost_benefit_per_video' do
       groups = GoogleAds::CostBenefit.new.per_video
       result = [
-          { id_video_principal: 'v1', cost_benefit: (((712 + (46.43 * 2) + (30.92 * 4) + (25.17 * 5) + (17.97 * 3))/15)/ 10.05).floor(2)},
-          { id_video_principal: 'v10', cost_benefit: (((595 + (29.519 * 2) + (17.27 * 4) + (10.07 * 5) + (5.5 * 3))/15)/ 6.96).floor(2)},
+          { id_video_principal: 'v1', cost_benefit: 7.35 },#((712 + (46.43 * 2) + (30.92 * 4) + (25.17 * 5) + (17.97 * 3))/15)/ 10.05).floor(2)
+          { id_video_principal: 'v10', cost_benefit: 7.56 },#(((595 + (29.519 * 2) + (17.27 * 4) + (10.07 * 5) + (5.5 * 3))/15)/ 6.96).floor(2)
       ]
       expect(true).to be groups.include? result[0]
       expect(true).to be groups.include? result[1]
