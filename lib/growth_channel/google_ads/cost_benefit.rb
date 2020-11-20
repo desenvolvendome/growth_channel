8692073e2ba35e306376722729e9fdabcd26e42b
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-11-19 22:17:04 -0300

    refactor(cost_benefit): refactor methods

--- lib/growth_channel/google_ads/cost_benefit.rb
+++ lib/growth_channel/google_ads/cost_benefit.rb
@@ -11,14 +11,24 @@ module GoogleAds
     def per_video(sort_increasing: false, filter_tag: '')
       group_videos = []
       group_main_video_info(filterTag: filter_tag).each do |video|
-        id_video_principal = video[:id_video_principal]
-        cost_benefit = video[:views] + (video[:watched_25] * 2) + (video[:watched_50] * 4) + (video[:watched_75] * 5) + (video[:watched_100] * 3)
-        cost_benefit /= 15 # soma dos pesos
-        cost_benefit /= video[:cost]
-        cost_benefit = cost_benefit.floor(2)
-        group_videos.push({id_video_principal: id_video_principal, cost_benefit: cost_benefit})
+        generate_score(group_videos, video)
       end
       sort_increasing ? group_videos.sort_by { |element| element[:cost_benefit] }.reverse : group_videos
+    end
+
+    private
+
+    def generate_score(group_videos, video)
+      id_video_principal = video[:id_video_principal]
+      cost_benefit = calculate_benefit_cost(video)
+      group_videos.push({id_video_principal: id_video_principal, cost_benefit: cost_benefit})
+    end
+
+    def calculate_benefit_cost(video)
+      cost_benefit = video[:views] + (video[:watched_25] * 2) + (video[:watched_50] * 4) + (video[:watched_75] * 5) + (video[:watched_100] * 3)
+      cost_benefit /= 15 # soma dos pesos
+      cost_benefit /= video[:cost]
+      cost_benefit.floor(2)
     end
   end
 end
