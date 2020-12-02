81d38de3d7a1c814971eb90c388fdc892894c7a1
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2020-12-01 22:00:07 -0300

    refactor(investment): refactor names of investments files

--- spec/google_ads/investment_spec.rb
+++ spec/google_ads/investment_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'GoogleAds::Investment' do
 
   context 'List' do
     it 'maiores_invenstimentos' do
-      groups = GoogleAds::Investment.new.maiores_invenstimentos('')
+      groups = GoogleAds::Investment.new.bigger_list('')
       result = [
           {id_video_principal: 'v7', cost: 77.52},
           {id_video_principal: 'v17', cost: 3.22}
