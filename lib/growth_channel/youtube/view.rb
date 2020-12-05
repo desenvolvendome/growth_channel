c99a2aecd4f6316ee38ecdda2197320fb07efad5
Arlisson Jaques <arlissonjaques@gmail.com>
2020-12-04 21:38:59 -0300

    refactor(view): added organic_view method in the view class

--- lib/growth_channel/youtube/view.rb
+++ lib/growth_channel/youtube/view.rb
@@ -1,9 +1,9 @@
-require 'csv'
-require 'growth_channel/util/manage_csv'
+require "csv"
+require "growth_channel/util/manage_csv"
+require "growth_channel/google_ads/views"
 
 module Youtube
   class View
-
     def initialize
       @report = ManageCSV.read_csv_youtube
     end
@@ -15,6 +15,27 @@ module Youtube
         total_views += views
       end
       total_views
+    end
+
+    def validate_subtraction(youtube, adsense)
+      youtube - adsense >= 0 ? true : nil
+    end
+
+    def organic_view
+      # todo: não consegui refatorar o código tão bem por enquanto.
+      subtracted_views = []
+      total_views_per_video.each do |hash_youtube|
+        GoogleAds::View.new.total_per_video.each do |hash_adsense|
+          if hash_youtube[:id_video_principal] == hash_adsense[:id_video_principal]
+            if validate_subtraction(hash_youtube[:views], hash_adsense[:views])
+              subtracted_views.push({id_video: hash_youtube[:id_video_principal], views: hash_youtube[:views] - hash_adsense[:views]})
+            else
+              subtracted_views.push({id_video: "#{hash_youtube[:id_video_principal]}: apresenta inconsistência nos dados", views: hash_youtube[:views] - hash_adsense[:views]})
+            end
+          end
+        end
+      end
+      subtracted_views
     end
 
     def total_views_per_video
@@ -45,6 +66,5 @@ module Youtube
     def videos_principais_agrupados
       read_per_video.group_by { |h| h[:id_video_principal] }.values
     end
-
   end
 end
