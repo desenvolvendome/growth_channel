b90baadd9b8b3aa8a140ed2f8b0018f0735c4b96
Marco Castro <marcodotcastro@gmail.com>
2020-12-07 22:07:35 -0300

    build(devise): adicionado html para adicionado stilo

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -111,6 +111,9 @@ Devise.setup do |config|
   # won't boot properly.
   # config.reload_routes = true
 
+  config.sign_out_via = :get
+
+
   # ==> Configuration for :database_authenticatable
   # For bcrypt, this is the cost for hashing the password and defaults to 12. If
   # using other algorithms, it sets how many times you want the password to be hashed.
